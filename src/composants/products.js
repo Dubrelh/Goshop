3f0d0eea8abaa0316992fad49f04e79959a08ed9
henry <henrykemeni0gmail.com>
2025-07-13 16:18:38 +0100

    update liens

--- src/composants/products.js
+++ src/composants/products.js
@@ -6,10 +6,10 @@ export const Produits = [
     image: "https://m.media-amazon.com/images/I/81poC2j8uvL._AC_UL480_FMwebp_QL65_.jpg",
     nom: "Tapis d’acupression",
     description: " Soulage les douleurs musculaires et lombaires",
-    prix: 43.99,
+    prix: 36.55,
     categorie: "Santé & Bien-être à domicile",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/4eSyFLo",
     commentaire: []
   },
   {
@@ -20,7 +20,7 @@ export const Produits = [
     prix: 99.99,
     categorie: "Santé & Bien-être à domicile",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/40MjM7i",
     commentaire: []
   },
   {
@@ -28,10 +28,10 @@ export const Produits = [
     image: "https://m.media-amazon.com/images/I/815p5Bps4gL._AC_UL480_FMwebp_QL65_.jpg",
     nom: "Purificateur d’air Levoit Core 300S",
     description: "Élimine 99% des allergènes et poussières",
-    prix: 149.99,
+    prix: 96.57,
     categorie: "Santé & Bien-être à domicile",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/4lSI2No",
     commentaire: []
   },
   {
@@ -39,21 +39,21 @@ export const Produits = [
     image: "https://m.media-amazon.com/images/I/51RR0csnVcL._AC_UL480_FMwebp_QL65_.jpg",
     nom: "PLampe de luminothérapie Beurer",
     description: "Simulation de lumière naturelle pour bien-être",
-    prix: 49.99,
+    prix: 60.47,
     categorie: "Santé & Bien-être à domicile",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/4nMXnki",
     commentaire: []
   },
   {
     id: "5",
-    image: "https://images-eu.ssl-images-amazon.com/images/I/71b9kZz1AiL._AC_UL330_SR330,330_.jpg",
+    image: "https://m.media-amazon.com/images/I/71MApfqsVoL._AC_UL480_FMwebp_QL65_.jpg",
     nom: "Coussin massant",
     description: "Simulation de lumière naturelle pour bien-être",
-    prix: 59.99,
+    prix: 45.12,
     categorie: "Santé & Bien-être à domicile",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/44r8E24",
     commentaire: []
   },
 
@@ -64,32 +64,32 @@ export const Produits = [
     image: "https://m.media-amazon.com/images/I/51SSRo2K+fL._AC_UL480_FMwebp_QL65_.jpg",
     nom: "Fontaine à eau automatique PETKIT",
     description: " Pour hydrater chats et petits chiens avec filtre intégré",
-    prix: 33,
+    prix: 29.19,
     categorie: "Produits pour animaux",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/44QEm7V",
     commentaire: []
   },
   {
     id: "7",
-    image: "https://m.media-amazon.com/images/I/61Oz5gPEtML._AC_UL480_FMwebp_QL65_.jpg",
+    image: "https://m.media-amazon.com/images/I/817MPAWc3VL._AC_UL480_FMwebp_QL65_.jpg",
     nom: "Arbre à chat FEANDREA",
     description: "Multi-niveaux, parfait pour les chats actifs",
-    prix: 53.03,
+    prix: 54.99,
     categorie: "Produits pour animaux",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/4eMwB7B",
     commentaire: []
   },     
     {
     id: "8",
-    image: "https://m.media-amazon.com/images/I/71u0pulOTiL._AC_UL480_FMwebp_QL65_.jpg",
+    image: "https://m.media-amazon.com/images/I/71QVTA4yGbL._AC_UL480_FMwebp_QL65_.jpg",
     nom: "Distributeur croquettes",
     description: "Automatique, programmable pour chats et petits chiens",
-    prix: 53.03,
+    prix: 29.99,
     categorie: "Produits pour animaux",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/4lP0JBg",
     commentaire: []
   }, 
   {
@@ -100,7 +100,7 @@ export const Produits = [
     prix: 23.99,
     categorie: "Produits pour animaux",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/40OcrEh",
     commentaire: []
   }, 
     {
@@ -111,7 +111,7 @@ export const Produits = [
     prix: 9.98,
     categorie: "Produits pour animaux",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/3GlryOM",
     commentaire: []
   }, 
 
@@ -125,18 +125,18 @@ export const Produits = [
     prix: 139.99,
     categorie: "Accessoires télétravail / bureau",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/44qOpl8",
     commentaire: []
   }, 
   {
     id: "12",
-    image: "https://m.media-amazon.com/images/I/71vK6sp4kZL._AC_SX679_.jpg",
+    image: "https://m.media-amazon.com/images/I/41xJfx9zKWL._AC_UL480_FMwebp_QL65_.jpg",
     nom: "Bras articulé double écran HUANUO",
     description: "Support pour deux écrans jusqu’à 27 pouces",
-    prix: 33.29,
+    prix: 39.99,
     categorie: "Accessoires télétravail / bureau",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/46DCT7g",
     commentaire: []
   }, 
   {
@@ -147,7 +147,7 @@ export const Produits = [
     prix: 26.99,
     categorie: "Accessoires télétravail / bureau",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/3U6TUPQ",
     commentaire: []
   }, 
   {
@@ -155,10 +155,10 @@ export const Produits = [
     image: "https://m.media-amazon.com/images/I/51ZG+F9K09L._AC_UL480_FMwebp_QL65_.jpg",
     nom: "Dock station USB-C Anker PowerExpand",
     description: "Connexions multiples pour PC/Mac",
-    prix: 59.22,
+    prix: 56.33,
     categorie: "Accessoires télétravail / bureau",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/3GxFVzx",
     commentaire: []
   }, 
   {
@@ -169,7 +169,7 @@ export const Produits = [
     prix: 24.95,
     categorie: "Accessoires télétravail / bureau",
     statut: "",
-    lien_affiliation: "https://www.amazon.fr/dp/B0CXXXXXX/?tag=TONIDPARTENAIRE",
+    lien_affiliation: "https://amzn.to/3GBCGXS",
     commentaire: []
   }, 
 
